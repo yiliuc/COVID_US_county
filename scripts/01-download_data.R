23578094fd6bdb0e5cd6398e4bf76892c3b53f3a
Yiliu Cao <l1023498789@gmail.com>
2023-10-21 22:50:26 -0400

    Update 01-download_data.R
    Now it contains all the raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -19,14 +19,21 @@ DP03 <- read.csv(url(url03), stringsAsFactors = FALSE)
 url05 <- "https://api.census.gov/data/2021/acs/acs5/profile?get=group(DP05)&for=county:*"
 DP05 <- read.csv(url(url05), stringsAsFactors = FALSE)
 
-url_covid <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-09-2023.csv"
+url_covid <- paste0(
+  "https://raw.githubusercontent.com/CSSEGISandData/",
+   "COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/",
+   "03-09-2023.csv")
 covid_data <- read.csv(url_covid, header = TRUE, sep = ",")
 
+url_election <- paste0(
+  "https://raw.githubusercontent.com/tonmcg/US_County_Level_Election_Results_08-20/",
+  "master/2020_US_County_Level_Presidential_Results.csv")
+election_data <- read.csv(url_election, header = TRUE, sep = ",")
 
 #### Save data ####
 write_csv(DP02, "inputs/data/DP_02.csv")
 write_csv(DP03, "inputs/data/DP_03.csv")
 write_csv(DP05, "inputs/data/DP_05.csv")
-write_csv(DP05, "inputs/data/covid_data.csv")
-
+write_csv(covid_data, "inputs/data/covid_data.csv")
+write_csv(election_data, "inputs/data/election_data.csv")
          
