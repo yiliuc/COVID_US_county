afc384dd32626921a5c24015488b88fecf33f418
Yiliu Cao <l1023498789@gmail.com>
2023-10-28 19:47:16 -0400

    Update 01-download_data.R
    Download the api change document

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -18,6 +18,8 @@ url03 <- "https://api.census.gov/data/2021/acs/acs5/profile?get=group(DP03)&for=
 DP03 <- read.csv(url(url03), stringsAsFactors = FALSE)
 url05 <- "https://api.census.gov/data/2021/acs/acs5/profile?get=group(DP05)&for=county:*"
 DP05 <- read.csv(url(url05), stringsAsFactors = FALSE)
+url06 <- "https://www2.census.gov/data/api-documentation/2021-5yr-api-changes.csv"
+api_change_document <- read.csv(url(url06), stringsAsFactors = FALSE)
 
 url_covid <- paste0(
   "https://raw.githubusercontent.com/CSSEGISandData/",
@@ -36,4 +38,5 @@ write.csv(DP03, "inputs/data/DP_03.csv", row.names = FALSE)
 write.csv(DP05, "inputs/data/DP_05.csv", row.names = FALSE)
 write.csv(covid_data, "inputs/data/covid_data.csv", row.names = FALSE)
 write.csv(election_data, "inputs/data/election_data.csv", row.names = FALSE)
-         
+write.csv(api_change_document, "inputs/data/api_change_document.csv", 
+          row.names = FALSE)
