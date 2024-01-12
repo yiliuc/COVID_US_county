6ed208bdc3f19845a33b86430c0a5e3e0c3da39f
Yiliu Cao <l1023498789@gmail.com>
2024-01-12 14:16:10 -0500

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,21 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulate the data
+# Author: Yiliu Cao
+# Date: 1 October 2023
+# Contact: yiliu.cao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: None
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
 
+data <- tibble(county = sample(c(1:3000)),
+               infection_rate = runif(3000, min = 0, max = 1),
+               death_rate = runif(3000, min = 0, max = 1),
+               insurance = runif(3000, min = 0, max = 1),
+               higher_education = runif(3000, min = 0, max = 0.5),
+               winning_party = sample(x = c("Democrat", "Republican"), size = 3000, replace = TRUE))
 
 
